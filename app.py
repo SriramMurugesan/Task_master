a513d7cdaf4e2be15bf0ab8c863ccb80f2200785
SriramMurugesan <sri2014ram@gmail.com>
2025-07-23 12:13:43 +0530

    flask intro

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/')
+    with app.app_context():
+        db.create_all()
+        
 @app.route('/')
 def index():
     return render_template('index.html')
