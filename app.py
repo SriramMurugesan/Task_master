146310016c1cc28f9201127a825d5e3e2ff8469e
SriramMurugesan <sri2014ram@gmail.com>
2025-07-23 12:22:56 +0530

    flask intro

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,17 +12,16 @@ class Task(db.Model):
     title = db.Column(db.String(100), nullable=False)
     completed = db.Column(db.Boolean, default=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
-    with app.app_context():
-        db.create_all()
-        
-@app.route('/')
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
