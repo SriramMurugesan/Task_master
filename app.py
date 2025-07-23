c2e36959990189048bbfc643e13892e9f2a02489
SriramMurugesan <sri2014ram@gmail.com>
2025-07-23 12:56:12 +0530

    flask completed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,7 +21,56 @@ with app.app_context():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Task(title=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    
+    tasks = Task.query.order_by(Task.date_created).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Task.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Task.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.title = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task)
+
+@app.route('/toggle/<int:id>')
+def toggle_complete(id):
+    task = Task.query.get_or_404(id)
+    task.completed = not task.completed
+    
+    try:
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an issue updating the task status'
 
 if __name__ == '__main__':
     app.run(debug=True)
